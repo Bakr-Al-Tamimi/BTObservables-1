9bf46320a115be7641b84c032dd6b36a8bda2883
altamimib <111327811+altamimib@users.noreply.github.com>
2023-05-07 20:20:16 +0930

    add cdk drag and drop component

--- src/app/cdk-examples/cdk-drag-drop-connected-sorting-example/cdk-drag-drop-connected-sorting-example.component.ts
+++ src/app/cdk-examples/cdk-drag-drop-connected-sorting-example/cdk-drag-drop-connected-sorting-example.component.ts
@@ -10,9 +10,9 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
   styleUrls: ['cdk-drag-drop-connected-sorting-example.component.css'],
 })
 export class CdkDragDropConnectedSortingExampleComponent {
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  todo = ['Get to work', 'Call Wife', 'Go home', 'Fall asleep'];
 
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Call Mum'];
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
